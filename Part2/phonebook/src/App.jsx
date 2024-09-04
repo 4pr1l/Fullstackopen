de3cee60f75d3e54c35a2b60b9b073c1ec843010
4pr1l <apriljack333@gmail.com>
2024-09-04 17:31:47 +0100

    part2
    part2

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import personService from './services/persons'
+import './index.css'
 const Form = (props) => {
   const handleVarChange = (event) =>{
     props.setVariable(event.target.value)
   }
   return (<>
-    {props.text}: <input value = {props.variable} onChange = {handleVarChange}/>
+    {props.text}: <input value = {props.variable} onChange = {handleVarChange} className='form'/>
   </> )
 }
 
-const Persons = ({persons, search}) => {
+const Persons = ({persons,search, deletePerson}) => {
   const filterPersons = () => {
     return ([...persons].filter(person => person.name.toLowerCase().includes(search.toLowerCase())))
   }
@@ -17,16 +18,19 @@ const Persons = ({persons, search}) => {
     <ul>
     {filterPersons().map(person =>
       <li key = {person.id}>
-        <Person fullName={person.name} number={person.number} />
+        <Person person={person}  deletePerson={deletePerson} />
       </li>
     )}
   </ul>
   )
 }
 
-const Person = ({fullName, number}) => {
+const Person = ({person, deletePerson}) => {
   return(
-    <p>Name: {fullName} <br></br>Number: {number}</p>
+    <>
+    <p>Name: {person.name} <br></br>Number: {person.number}</p> 
+    <button onClick={() => deletePerson(person)}>delete</button>
+    </>
   )
 }
 
@@ -35,30 +39,55 @@ const App = () => {
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [id, setId] = useState(1)
+  const [id, setId] = useState(50)
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', 
+    { name: 'Didnt get the server data', 
       number: '07942124352',
       id: 0
     }
   ]) 
 
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(initialNotes => {
+        setPersons(initialNotes)
+      })
+  }, [])
+
+  console.log('render', persons.length, 'persons')
+
+  const deletePerson = person=>{
+    if (confirm(`Are you sure you want to delete ${person.name}` )){
+      personService.deleteObject(person.id).
+      then(
+        setPersons(persons.filter(p=> p.id!==person.id)),
+        console.log(...persons)
+
+      ).catch(error=>{
+        console.log('Person already deleted'),
+        alert('Person already deleted'),
+        setPersons(persons.filter(p=> p.id!==person.id)),
+        console.log(...persons)
+      }
+      )
+    }
+  }
   const addNewName = (event)=>{
     event.preventDefault()
     if (!persons.some((person)=>person.name===newName) && !persons.some((person)=>person.number===newNumber)){
       setId(id+1)
-      setPersons(persons.concat(
-        {
-        name: newName,
-        number: newNumber,
-        id: id
+      const newPerson = {
+      name: newName,
+      number: newNumber,
+      id: id.toString()
       }
-    )
-  )
-      setNewName('')
-      setNewNumber('')      
-  }
+      personService.create(newPerson).then(      
+        setNewName(''),
+        setNewNumber('')),
+        setPersons(persons.concat(newPerson))  }
   else {
     alert(`Name ${newName} or number ${newNumber} is already in the phonebook`)
   }
@@ -80,7 +109,7 @@ const App = () => {
       </div>
 
       <h2>Numbers</h2>
-      <Persons persons={persons} search={search}></Persons>
+      <Persons persons={persons} search={search} deletePerson={deletePerson}></Persons>
       ...
     </div>
     
